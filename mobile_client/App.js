2c22e840713a69d79369eac14ab9c85e97379e25
hugo <hugo@x-squad.com>
2020-04-30 12:55:08 +0200

    wait for serveur

--- mobile_client/App.js
+++ mobile_client/App.js
@@ -24,9 +24,66 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import axios from 'axios';
+
 import { Image } from 'react-native';
 
+let total_confirmed = "3 204 705"
+let total_death = "227 723"
+let total_pays = "185 "
+
+let pays1 = "US"
+let death1 = "60 999"
+let confirmed1 = "1 040 488"
+
+let pays2 = "Spain"
+let death2 = "24 275"
+let confirmed2 = "236 899"
+
+let pays3 = "Italy"
+let death3 = "27 682"
+let confirmed3 = "203 591"
+
+let pays4 = "France"
+let death4 = "24 121"
+let confirmed4 = "166 543"
+
+let pays5 = "UK"
+let death5 = "26 166"
+let confirmed5 = "166 441"
+
+let pays6 = "Germany"
+let death6 = "6 467"
+let confirmed6 = "161 539"
+
+let pays7 = "Turkey"
+let death7 = "3 081"
+let confirmed7 = "117 589"
+
+let pays8 = "Russia"
+let death8 = "1 073"
+let confirmed8 = "106 498"
+
+let pays9 = "Iran"
+let death9 = "5 957"
+let confirmed9 = "93 657"
+
+let pays10 = "China"
+let death10 = "4 637"
+let confirmed10 = "83 944"
+
+
 const App: () => React$Node = () => {
+  axios.get(('http://10.0.2.2:3000/api/stuff'), res => {
+
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch(function(error) {
+    console.log('There has been a problem with your fetch operation: ' + error.message);
+    throw error;
+  });
   return (
     <ScrollView>
           <View>
@@ -40,66 +97,105 @@ const App: () => React$Node = () => {
               source={require('./map.jpg')}
             />
           </View>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionTitle}>{total_pays} contry</Text>
+            <Text style={styles.sectionDescription}>
+              <Text style={styles.highlight}>{total_confirmed} confirmed</Text>
+            </Text>
+            <Text style={styles.sectionDescription}>
+              <Text style={styles.highlight}>{total_death} total_death</Text>
+            </Text>
+          </View>
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>pays1</Text>
+              <Text style={styles.sectionTitle}>{pays1}</Text>
               <Text style={styles.sectionDescription}>
-                <Text style={styles.highlight}>confirmed, death</Text>
+                <Text style={styles.highlight}>{confirmed1} confirmed</Text>
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{death1} death</Text>
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>pays2</Text>
-            <Text style={styles.sectionDescription}>
-              <Text style={styles.highlight}>confirmed, death</Text>
-            </Text>
+              <Text style={styles.sectionTitle}>{pays2}</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{confirmed2} confirmed</Text>
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{death2} death</Text>
+              </Text>
             </View>
             <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>pays3</Text>
-            <Text style={styles.sectionDescription}>
-              <Text style={styles.highlight}>confirmed, death</Text>
-            </Text>
+              <Text style={styles.sectionTitle}>{pays3}</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{confirmed3} confirmed</Text>
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{death3} death</Text>
+              </Text>
             </View>
             <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>pays4</Text>
-            <Text style={styles.sectionDescription}>
-              <Text style={styles.highlight}>confirmed, death</Text>
-            </Text>
+              <Text style={styles.sectionTitle}>{pays4}</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{confirmed4} confirmed</Text>
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{death4} death</Text>
+              </Text>
             </View>
             <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>pays5</Text>
-            <Text style={styles.sectionDescription}>
-              <Text style={styles.highlight}>confirmed, death</Text>
-            </Text>
+              <Text style={styles.sectionTitle}>{pays5}</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{confirmed5} confirmed</Text>
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{death5} death</Text>
+              </Text>
             </View>
             <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>pays6</Text>
-            <Text style={styles.sectionDescription}>
-              <Text style={styles.highlight}>confirmed, death</Text>
-            </Text>
+              <Text style={styles.sectionTitle}>{pays6}</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{confirmed6} confirmed</Text>
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{death6} death</Text>
+              </Text>
             </View>
             <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>pays7</Text>
-            <Text style={styles.sectionDescription}>
-              <Text style={styles.highlight}>confirmed, death</Text>
-            </Text>
+              <Text style={styles.sectionTitle}>{pays7}</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{confirmed7} confirmed</Text>
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{death7} death</Text>
+              </Text>
             </View>
             <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>pays8</Text>
-            <Text style={styles.sectionDescription}>
-              <Text style={styles.highlight}>confirmed, death</Text>
-            </Text>
+              <Text style={styles.sectionTitle}>{pays8}</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{confirmed8} confirmed</Text>
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{death8} death</Text>
+              </Text>
             </View>
             <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>pays9</Text>
-            <Text style={styles.sectionDescription}>
-              <Text style={styles.highlight}>confirmed, death</Text>
-            </Text>
+              <Text style={styles.sectionTitle}>{pays9}</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{confirmed9} confirmed</Text>
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{death9} death</Text>
+              </Text>
             </View>
             <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>pays10</Text>
-            <Text style={styles.sectionDescription}>
-              <Text style={styles.highlight}>confirmed, death</Text>
-            </Text>
+              <Text style={styles.sectionTitle}>{pays10}</Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{confirmed10} confirmed</Text>
+              </Text>
+              <Text style={styles.sectionDescription}>
+                <Text style={styles.highlight}>{death10} death</Text>
+              </Text>
             </View>
           </View>
         </ScrollView>
